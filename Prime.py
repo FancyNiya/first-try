a0a9da46e46302fa1da2a6656fa00c57361b04e5
wangjiayi1126 <38268733+wangjiayi1126@users.noreply.github.com>
2018-05-06 22:02:36 +0800

    Delete Prime.py

--- Prime.py
+++
@@ -1,9 +0,0 @@
-def Prime(n):    
-    if n <= 1:    
-        return False   
-    i = 2   
-    while i*i <= n:    
-        if n % i == 0:    
-            return False   
-        i += 1   
-    return True   
