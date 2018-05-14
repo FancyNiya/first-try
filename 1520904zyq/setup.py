d21a0c4ed9877237d78fb23bd8bdf90ae0c961f1
yanqiuzhao <38251689+yanqiuzhao@users.noreply.github.com>
2018-05-14 10:44:32 +0800

    Add files via upload

---
+++ 1520904zyq/setup.py
@@ -0,0 +1,8 @@
+from distutils.core import setup  
+setup(name='Myblog',
+      version='1.0',    
+      description='My Blog Distribution Utilities',    
+      author=' Myblog ',    
+      author_email='ZYQ09@gmail.com',   
+      py_modules=['py'],
+)    
